410fdddce285189d252573b0f67be7f4c4341132
Daniel Brice <danielbrice@gmail.com>
2015-02-21 12:58:02 -0600

    added a check to see if  exists. creates it if not.

--- scrape.sh
+++ scrape.sh
@@ -2,9 +2,13 @@
 
 STREAM="rtsp://68.152.51.100/axis-media/media.amp" # source stream
 FREQ="1" # frequency in seconds
-OUT_DIR="./out" # no trailing slash
+OUT_DIR="./images" # no trailing slash
 
 TIME=$(date +%s) # gets the system time in seconds
+
+if [ -d "OUT_DIR" ]; then
+	mkdir $OUT_DIR
+fi
 
 avconv -i $STREAM -r $FREQ -vsync 1 -qscale 1 -f image2 ${OUT_DIR}/${TIME}-%09d.jpg
 
