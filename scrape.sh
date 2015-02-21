fbbc1affdefc427f53acb984b3e80b892b3a5957
Daniel Brice <danielbrice@gmail.com>
2015-02-21 12:38:51 -0600

    modified scrape.sh to include the system time at the start of the scrape

--- scrape.sh
+++ scrape.sh
@@ -4,5 +4,7 @@ STREAM="rtsp://68.152.51.100/axis-media/media.amp" # source stream
 FREQ="1" # frequency in seconds
 OUT_DIR="./out" # no trailing slash
 
-avconv -i $STREAM -r $FREQ -vsync 1 -qscale 1 -f image2 ${OUT_DIR}/images%09d.jpg
+TIME=$(date +%s) # gets the system time in seconds
+
+avconv -i $STREAM -r $FREQ -vsync 1 -qscale 1 -f image2 ${OUT_DIR}/${TIME}-%09d.jpg
 
