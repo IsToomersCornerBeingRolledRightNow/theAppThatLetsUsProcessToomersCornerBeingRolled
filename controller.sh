b969f6b12a92a4bcd9390093de79dbe0b67d41a1
Daniel Brice <danielbrice@gmail.com>
2015-02-21 19:59:23 -0600

    i think controller.sh is more or less done.

--- controller.sh
+++ controller.sh
@@ -3,7 +3,7 @@
 myStream="rtsp://68.152.51.100/axis-media/media.amp"
 myImage="./tmp/image.bmp"
 myProcessor="./compare.sh"
-myAction="./tweet.sh"
+myAction="./tweet.rb"
 mySleep="1"
 
 function grab {
@@ -15,10 +15,9 @@ function grab {
   fi
   
   avconv -i "$1" -t 1 -r 1 -vsync 1 -qscale 1 -f image2 "$2"
-  return 0
 }
 
-function compare {
+function process {
   # $1 is the image processor
   # $2 is the captured image
   
@@ -31,27 +30,44 @@ function compare {
   fi
   
   echo $myReturn
-  return 0
 }
 
-if [[ -d ./tmp ]]; then
-	rm -rf ./tmp
-fi
-mkdir ./tmp
+function act {
+  # $1 is the action to be taken
+  # $2 is the captured image
 
-myOldState="unrolled"
-
-while true; do
-  $myGrabber $myStream $myImage
+  myOldImage="$2"
+  myNewImage="${myOldImage/%.bmp/.png}"
   
-  myNewState=$($myProcessor $myImage)
-    
-  if [[ $myOldState == "unrolled" && $myNewState == "rolled" ]]; then
-    $myAction $myImage &
+  if [[ -f $myNewImage ]]; then
+    rm $myNewImage
   fi
   
-  myOldState=$myNewState
-  sleep $mySleep
-  rm $myImage
-done
+  avconv -i $myOldImage $myNewImage
+  $1 $myNewImage
+}
+
+function main {
+
+  if [[ -d ./tmp ]]; then
+    rm -rf ./tmp
+  fi
+  mkdir ./tmp
+
+  myOldState="unrolled"
+
+  while true; do
+    grab $myStream $myImage
+    process $myProcessor $myImage
+    if [[ $myOldState == "unrolled" && $myNewState == "rolled" ]]; then
+      act $myAction $myImage &
+    fi
+    
+    myOldState=$myNewState
+    sleep $mySleep
+    rm $myImage
+  done
+}
+
+main
 
