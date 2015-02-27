f2be3a8442d19e8481caec2ac309fc38e6df36f9
Daniel Brice <danielbrice@gmail.com>
2015-02-26 20:45:53 -0600

    removed the sample data from the repository

--- testing-tools/intermitent-connection-scraper.sh
+++ testing-tools/intermitent-connection-scraper.sh
@@ -5,15 +5,18 @@
 
 stream="rtsp://68.152.51.100/axis-media/media.amp" # source stream
 freq="15" # frequency in seconds
-grabs="10" # number of frames to grab
+# grabs="10" # number of frames to grab
 
-i="0"
+#i="0"
 
-while [[ $i -lt $grabs ]]; do
+#while [[ $i -lt $grabs ]]; do
+while [[ true ]]; do
 
   timestamp=$(date +%s) # gets the system time in seconds
 
-  avconv -i "$stream" -t 1 -r 1 -vsync 1 -qscale 1 -f image2 ./intermitent/${timestamp}-%09d.jpg
+  avconv -i "$stream" -t 1 -r 1 -vsync 1 -qscale 1 -f image2 ./intermitent/${timestamp}.png
   
-  let i=i+1
+  #let i=i+1
+  sleep 15
 done
+
